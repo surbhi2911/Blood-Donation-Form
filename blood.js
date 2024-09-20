e51c87816356d9e56a186e7a9766fde700c57d0e
surbhi <surbhipansuriya633@gmail.com>
2024-09-20 09:10:23 +0530

    radio button code

--- blood.js
+++ blood.js
@@ -3,8 +3,34 @@ const submitForm = () => {
   let err = {};
   let input = document.querySelectorAll(".input");
   let select = document.querySelectorAll("select");
-  let radio = document.querySelector("input[type=radio]:checked")?.value;
-  let sdp = document.querySelector("input[name=SDP]:checked")?.value;
+  // let radio = document.querySelector("input[type=radio]:checked")?.value;
+  // let sdp = document.querySelector("input[name=SDP]:checked")?.value;
+
+  let radioButtons = document.querySelectorAll("input[type=radio]");
+  let radio = "";
+
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      radio = radioButtons[i].value;
+      break; // Exit the loop once the checked radio is found
+    }
+  }
+
+  // console.log(radio)
+  ;
+
+  let sdpButtons = document.querySelectorAll("input[name=SDP]");
+  let sdp = "";
+
+  for (let i = 0; i < sdpButtons.length; i++) {
+    if (sdpButtons[i].checked) {
+      sdp = sdpButtons[i].value;
+      break; // Exit the loop once the checked radio button is found
+    }
+  }
+
+  console.log(sdp); // This will print the value of the checked radio button with name "SDP"
+
 
   // Perform input validation
   input.forEach((x) => {
@@ -94,3 +120,175 @@ const submitForm = () => {
     });
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const submitForm = () => {
+//   let obj = {};
+//   let err = {};
+//   let input = document.querySelectorAll(".input");
+//   let select = document.querySelectorAll("select");
+//   let radio = document.querySelector("input[type=radio]:checked")?.value;
+//   let sdp = document.querySelector("input[name=SDP]:checked")?.value;
+
+//   // Perform input validation
+//   input.forEach((x) => {
+//     if (x.value === "") {
+//       document.querySelectorAll("span").forEach((y) => {
+//         if (x.name === y.id) {
+//           y.innerHTML = `${x.name} is required !`;
+//           err[x.name] = y.innerHTML;
+//         }
+//       });
+//     } else {
+//       document.querySelectorAll("span").forEach((y) => {
+//         if (x.name === y.id) {
+//           y.innerHTML = ``;
+//           err[x.name] = '';
+//         }
+//       });
+//       obj[x.name] = x.value;
+//     }
+//   });
+
+//   // Perform select validation
+//   select.forEach((x) => {
+//     if (x.value === "") {
+//       document.querySelectorAll(".select").forEach((y) => {
+//         if (x.name === y.id) {
+//           y.innerHTML = `${x.name} is required !`;
+//           err[x.name] = y.innerHTML;
+//         }
+//       });
+//     } else if (x.value) {
+//       document.querySelectorAll(".select").forEach((y) => {
+//         if (x.name === y.id) {
+//           y.innerHTML = ``;
+//           err[x.name] = "";
+//         }
+//       });
+//       obj[x.name] = x.value;
+//     }
+//   });
+
+//   // Perform radio button validation
+//   if (!radio) {
+//     document.getElementById("lastdonate").innerHTML =
+//       "Please select any of the options above";
+//     err.lastdonate = "Please select any of the options above";
+//   } else {
+//     document.getElementById("lastdonate").innerHTML = "";
+//     if (radio !== undefined) {
+//       obj["last-3-month-donate"] = radio;
+//       err.lastdonate = "";
+//     }
+//   }
+
+//   // Perform SDP validation
+//   if (!sdp) {
+//     document.getElementById("sdp").innerHTML =
+//       "Please select any of the options above";
+//     err.sdp = "Please select any of the options above";
+//   } else {
+//     document.getElementById("sdp").innerHTML = "";
+//     if (sdp !== undefined) {
+//       obj["SDP"] = sdp;
+//       err.sdp = "";
+//     }
+//   }
+
+//   console.log("Form submitted successfully:", obj);
+
+//   const hasErrors = Object.values(err).some((x) => x !== '');
+//   // Send form data to the API endpoint
+//   if (!hasErrors) {
+//     $.ajax({
+//       url: "https://api.apispreadsheets.com/data/FHShrM4vadQQaHSz/",
+//       type: "post",
+//       data: $("#form").serializeArray(),
+//       success: function () {
+//         document.getElementById("form").reset();
+//         document.getElementById("abc").style.display = "block";
+//         setTimeout(() => {
+//           document.getElementById("abc").style.display = "block";
+//         }, 10000);
+//       },
+//       error: function () {
+//         alert("There was an error :(");
+//       },
+//     });
+//   }
+// };
